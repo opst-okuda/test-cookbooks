11e7359346fd229f4fdd5b736b50873d6b2048fd
opst-okuda <okuda.h@opst.co.jp>
2015-01-05 05:36:03 +0000

    add cookbook for nginx-jenkins

---
+++ nginx-jenkins/recipes/default.rb
@@ -0,0 +1,36 @@
+package "nginx" do
+	action :install
+end
+
+directory node[:nginx][:dir] do
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+directory node[:nginx][:log_dir] do
+  mode 0755
+  owner node[:nginx][:user]
+  action :create
+end
+
+template "#{node[:nginx][:dir]}/nginx.conf" do
+	source "nginx.conf.erb"
+	owner "root"
+	group "root"
+	mode 0644
+	notifiles :reload, "service[nginx]"
+end
+
+template "#{node[:nginx][:dir]}/conf.d/jenkins.conf" do
+	source "jenkins-site.erb"
+	owner "root"
+	group "root"
+	mode 0644
+	notifiles :reload, "service[nginx]"
+end
+
+service "nginx" do
+	supports status: true, restart: true, reload:true
+	action [:enable, :start]
+end
